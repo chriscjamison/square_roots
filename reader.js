6a1dee2a8f48f5e80abca0e69f2be5a1fb5e3e57
Chris Jamison <chris@chriscjamison.com>
2016-11-01 21:32:50 -0700

    0.0.2 - CJ - Picks out metadata from okp.php * elements_searched_Array is too long for okp.php * Will be pruned down to extract advertisment code

--- reader.js
+++ reader.js
@@ -18,7 +18,7 @@ $(document).ready(
       var data_found_Array = new Array();
       // Holds the board metadata which will be inserted into an HTML template.
 
-      var inc, inc_2 = new Number();
+      var inc, inc_2, inc_3 = new Number();
       // Hold incrementers.
 
       var search_ele_nums_Array = new Array();
@@ -27,6 +27,9 @@ $(document).ready(
       var next_ele_vals_Array = new Array();
       // Contains the elements_searched_Array index number that matches the string to be searched for.
 
+      var html_snippet = new String();
+      // Contains metadata from the HTML which will be reinserted into updated, modern HTML5.
+
       elements_searched_Array = [
         "<td class=\"dcmenu\">", 
         "<a href=\"", 
@@ -34,6 +37,9 @@ $(document).ready(
         "</a>",
         "<img src=\"", 
         "\"",
+        "<span class=\"dcemp\">",
+        "</span>",
+        "<span class=\"dcdate\">",
         "<table class=\"dcborder\"",
         "<td class=\"dcheading\" colspan=\"6\">",
         "</td>",
@@ -48,7 +54,6 @@ $(document).ready(
         "<b>",
         "<a name=\"",
         "<span class=\"dclink\">",
-        "</span>",
         "\">",
         "<td class=\"dcdate\"  width=\"120\">",
         "<div align=\"center\">",
@@ -57,62 +62,69 @@ $(document).ready(
         "</A>"
       ];
       
-      search_ele_nums_Array = [0, 1, 1];
+      search_ele_nums_Array = [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
-      next_ele_vals_Array = [0, 1, 2, 4];
+      next_ele_vals_Array = [0, 1, 2, 3, 4, 5, 1, 2, 4, 5, 1, 2, 1, 2, 4, 5, 1, 2, 1, 2, 4, 5, 1, 2, 1, 2, 4, 5, 6, 7, 8, 7];
       
       inc = 0;
+      inc_3 = 0;
       next_ele_index_val = 0;
 
       html_source_code = $("html").html();
       // window.alert("search_ele_nums_Array.length = " + search_ele_nums_Array.length);
-      while (inc <= search_ele_nums_Array.length)  {
+      for (inc = 0; inc < search_ele_nums_Array.length; inc++)  {
         // window.alert("inc = " + inc);
-        window.alert("elements_searched_Array[" + next_ele_vals_Array[inc] + "] = " + elements_searched_Array[next_ele_vals_Array[inc]]);
-        current_index_val = html_source_code.indexOf(elements_searched_Array[next_ele_vals_Array[inc]], next_ele_index_val);
-        //  window.alert("current_index_val = " + current_index_val);
-        current_index_val = current_index_val + elements_searched_Array[next_ele_vals_Array[inc]].length;
+        // window.alert("elements_searched_Array[" + next_ele_vals_Array[inc_3] + "] = " + elements_searched_Array[next_ele_vals_Array[inc_3]]);
+        current_index_val = html_source_code.indexOf(elements_searched_Array[next_ele_vals_Array[inc_3]], next_ele_index_val);
+        current_index_val = current_index_val + elements_searched_Array[next_ele_vals_Array[inc_3]].length;
         // window.alert("current_index_val = " + current_index_val);
-        // window.alert("inc = " + inc);
+        
+        inc_3++;
+      
+        // window.alert("inc_3 = " + inc_3);
         if (search_ele_nums_Array[inc] > 0) {
-          inc_2 = inc;
-          window.alert("inc_2 = " + inc_2);
-          while (inc_2 <= search_ele_nums_Array[inc_2])  {
-            if (inc_2 === inc) {
-              inc_2++;
-            }
+          // window.alert("inc = " + inc);
+          // window.alert("search_ele_nums_Array[" + inc + "] = " + search_ele_nums_Array[inc]);
+
+          for (inc_2 = 0; inc_2 < search_ele_nums_Array[inc]; inc_2++) {
             // window.alert("inc = " + inc);
-            window.alert("inc_2 = " + inc_2);
-            // window.alert("search_ele_nums_Array[" + inc + "] = " + search_ele_nums_Array[inc_2]);
-            window.alert("next_ele_vals_Array[" + inc_2 + "] = " + next_ele_vals_Array[inc_2]);
-            window.alert("elements_searched_Array[" + next_ele_vals_Array[inc_2] + "] = " + elements_searched_Array[next_ele_vals_Array[inc_2]]);
+            // window.alert("inc_2 = " + inc_2);
+            // window.alert("inc_3 = " + inc_3);
+            
+            next_ele_index_val = html_source_code.indexOf(elements_searched_Array[next_ele_vals_Array[inc_3]], current_index_val);
+
+            // next_ele_index_val = next_ele_index_val + elements_searched_Array[next_ele_vals_Array[inc_3]].length;
             // window.alert("next_ele_index_val = " + next_ele_index_val);
+            html_snippet = html_source_code.slice(current_index_val, next_ele_index_val);
+            // window.alert("html_source_code.slice(" + current_index_val + ", " + next_ele_index_val + ") = " + html_source_code.slice(current_index_val, next_ele_index_val));
+            data_found_Array.push(html_snippet);
+            // next_ele_index_val = current_index_val;
+            // window.alert("inc_2 = " + inc_2);
+            // window.alert("next_ele_vals_Array[" + inc_3 + "] = " + next_ele_vals_Array[inc_3]);
+            
+            // window.alert("elements_searched_Array[" + next_ele_vals_Array[inc_3] + "] = " + elements_searched_Array[[next_ele_vals_Array[inc_3]]]);
+
             // window.alert("current_index_val = " + current_index_val);
-            if (current_index_val < next_ele_index_val) {
+            if (inc_2 < search_ele_nums_Array[inc]) {
               current_index_val = next_ele_index_val;
-            }
-            //  window.alert("current_index_val = " + current_index_val);
-            next_ele_index_val = html_source_code.indexOf(elements_searched_Array[next_ele_vals_Array[inc_2]], current_index_val);
-            // window.alert("next_ele_index_val = " + next_ele_index_val);
 
-            data_found_Array.push(html_source_code.slice(current_index_val, next_ele_index_val));
-
-            window.alert("data_found_Array[" + (data_found_Array.length - 1) + "] = " + data_found_Array[(data_found_Array.length - 1)]);
-            //  window.alert("current_index_val = " + current_index_val);
-            current_index_val = next_ele_index_val;
-            //  window.alert("next_ele_index_val = " + next_ele_index_val);
-
+              // current_index_val = current_index_val + elements_searched_Array[next_ele_vals_Array[inc_3]].length;
+            }           
+            
+            
+            inc_3++;
+            
+            
+            // window.alert("current_index_val = " + current_index_val);
           }
         }
 
         if (next_ele_index_val < current_index_val) {
           next_ele_index_val = current_index_val;
         }
-        // window.alert("next_ele_index_val = " + next_ele_index_val);
-        inc++;
       }
 
-      
+      // window.alert("data_found_Array = " + data_found_Array);
     }
     
     scrapeHTML();
